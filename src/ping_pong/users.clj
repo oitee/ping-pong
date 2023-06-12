6b48457a0ffca8bbecaf1143616fa80ad16988d3
oitee <oitee.codes@gmail.com>
2023-06-12 07:36:27 +0530

    Refactor users and utils ns

--- src/ping_pong/users.clj
+++ src/ping_pong/users.clj
@@ -1,25 +1,8 @@
 (ns ping-pong.users
-  (:require [clojure.string :as cs]))
+  (:require [clojure.string :as cs]
+            [ping-pong.utils :refer [user-names email-domains]]))
 
-;; List of The Office (US) characters.
-;; Taken from: https://en.wikipedia.org/wiki/List_of_The_Office_(American_TV_series)_characters
-(def user-names
-  ["Michael Scott" "Dwight Schrute" "Jim Halpert" "Pam Beesly" "Ryan Howard"
-   "Andy Bernard" "Robert California" "Stanley Hudson" "Kevin Malone"
-   "Meredith Palmer" "Angela Martin" "Oscar Martinez" "Phyllis Lappin" "Roy Anderson"
-   "Jan Levinson" "Toby Flenderson" "Kelly Kapoor" "Creed Bratton" "Darryl Philbin"
-   "Erin Hannon" "Gabe Lewis" "Holly Flax" "Nellie Bertram" "Clark Green"
-   "Pete Miller" "Devon White" "Lonny Collins" "Katy Moore" "Todd Packer" "Hank Doyle"
-   "Carol Stills" "Bob Vance" "David Wallace" "Mose Schrute" "Helene Beesly"
-   "Josh Porter" "Karen Filippelli" "Nick Lynn" "Charles Miner" "Isabel Poreba"
-   "Matt Donna" "Jo Bennet" "Robert Lipton" "Nate Nickerson" "Deangelo Vickers"
-   "Jordan Garfield" "Val Johnson" "Cathy Simms" "Jessica" "Irene" "Brian"
-   "Ziek Schrute" "Esther Breugger"])
-
-(def email-domains
-  ["gmail.com" "hotmail.com" "yahoo.com" "aol.com" "msn.com" "live.com" "rediffmail.com"])
-
-(def users (atom []))
+(defonce users (atom []))
 
 (defn- get-users
   []
